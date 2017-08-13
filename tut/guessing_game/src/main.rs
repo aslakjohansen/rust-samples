5037c0ed908a2834d7cf2e81ab871674979e3a19
Aslak Johansen <aslakj@gmail.com>
2017-08-13 12:28:43 +0200

    Fourth part of the guessing game

--- tut/guessing_game/src/main.rs
+++ tut/guessing_game/src/main.rs
@@ -10,21 +10,23 @@ fn main () {
     let secret_number = rand::thread_rng().gen_range(1, 101);
     println!("The secret number is {}", secret_number);
     
-    println!("Your guess?");
-    
-    let mut guess = String::new();
-    
-    io::stdin().read_line(&mut guess)
-               .expect("Failed to read line");
-    println!("Your guess: {}", guess);
-    
-    let guess: u32 = guess.trim().parse().expect("That wasn't very numbery");
-    println!("Your guess: {}", guess);
-    
-    match guess.cmp(&secret_number) {
-        Ordering::Less    => println!("Too small"),
-        Ordering::Greater => println!("Too big"),
-        Ordering::Equal   => println!("Too equal"),
+    loop {
+        println!("Your guess?");
+        
+        let mut guess = String::new();
+        
+        io::stdin().read_line(&mut guess)
+                   .expect("Failed to read line");
+        println!("Your guess: {}", guess);
+        
+        let guess: u32 = guess.trim().parse().expect("That wasn't very numbery");
+        println!("Your guess: {}", guess);
+        
+        match guess.cmp(&secret_number) {
+            Ordering::Less    => println!("Too small"),
+            Ordering::Greater => println!("Too big"),
+            Ordering::Equal   => println!("Too equal"),
+        }
     }
 }
 
