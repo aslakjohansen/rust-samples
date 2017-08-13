a5e16cb5c86346e677f5fc7bd7a2e560fbe8bb65
Aslak Johansen <aslakj@gmail.com>
2017-08-13 12:26:21 +0200

    Third part of the guessing game

--- tut/guessing_game/src/main.rs
+++ tut/guessing_game/src/main.rs
@@ -1,6 +1,7 @@
 extern crate rand;
 
 use std::io;
+use std::cmp::Ordering;
 use rand::Rng;
 
 fn main () {
@@ -15,7 +16,15 @@ fn main () {
     
     io::stdin().read_line(&mut guess)
                .expect("Failed to read line");
-    
     println!("Your guess: {}", guess);
+    
+    let guess: u32 = guess.trim().parse().expect("That wasn't very numbery");
+    println!("Your guess: {}", guess);
+    
+    match guess.cmp(&secret_number) {
+        Ordering::Less    => println!("Too small"),
+        Ordering::Greater => println!("Too big"),
+        Ordering::Equal   => println!("Too equal"),
+    }
 }
 
