a7acd5ac1d5e0a9b6005402be5828b039d5037bf
Aslak Johansen <aslakj@gmail.com>
2017-11-10 16:44:17 +0100

    Got around first set of compile errors

--- projects/gameoflife/src/main.rs
+++ projects/gameoflife/src/main.rs
@@ -1,71 +1,81 @@
 struct World {
     width: usize,
     height: usize,
-    data: [bool],
+    buffer: [bool],
 }
 
-fn load (filename) {
-    data = World(7, 4,
-                 [false, true , false, false, false, false, false,
-                  false, true , false, true , true , true , false,
-                  false, true , false, false, false, false, false,
-                  false, false, false, false, false, false, false]);
-    return data;
+fn load (filename: String) {
+    world = World(7, 4,
+                  [false, true , false, false, false, false, false,
+                   false, true , false, true , true , true , false,
+                   false, true , false, false, false, false, false,
+                   false, false, false, false, false, false, false]);
+    return world;
 }
 
-fn blank (source) {
-    data = World(source.width, source.height,
-                 [false; source.width * source.height])
+fn blank (source: World) {
+    world = World(source.width, source.height,
+                  [false; source.width * source.height]);
+    return world;
 }
 
-fn index (buffer, x, y) {
-    return buffer.data[buffer.width*y + x]
+fn index (world: World, x: usize, y: usize) {
+    return world.buffer[world.width*y + x];
 }
 
-fn assign (buffer, x, y, value) {
-    buffer.data[buffer.width*y + x] = value;
+fn assign (world: World, x: usize, y: usize, value: bool) {
+    world.buffer[world.width*y + x] = value;
 }
 
-fn show (buffer) {
+fn show (world: World) {
     println!("");
     println!("");
     
     // print top frame
     print!("+");
-    for x in ..buffer.width {
+    for x in ..world.width {
         print!("-");
     }
     println!("+");
     
     // print body
     print!("|");
-    for y in ..buffer.height {
-        for x in ..buffer.width {
-            print!(if index(buffer, x, y) {"X"} else {" "});
+    for y in ..world.height {
+        for x in ..world.width {
+//            print!(if index(world, x, y) {"X"} else {" "});
+//            print!(match index(world, x, y) {
+//                b if b==true  => "X",
+//                b if b==false => " ",
+//            } as String);
+            s: String = match index(world, x, y) {
+                b if b==true  => "X",
+                b if b==false => " ",
+            };
+            print!("{}", s);
         }
     }
     println!("|");
     
     // print bottom frame
     print!("+");
-    for x in ..buffer.width {
+    for x in ..world.width {
         print!("-");
     }
     println!("+");
 }
 
-fn iterate (source, destination) {
-    for y in ..buffer.height {
-        for x in ..buffer.width {
-            count: usize = index(buffer, x-1, y-1) + index(buffer, x, y-1) + index(buffer, x+1, y-1)
-                         + index(buffer, x-1, y  ) + index(buffer, x, y  ) + index(buffer, x+1, y  )
-                         + index(buffer, x-1, y+1) + index(buffer, x, y+1) + index(buffer, x+1, y+1);
+fn iterate (source: world, destination: world) {
+    for y in ..source.height {
+        for x in ..source.width {
+            count: usize = index(source, x-1, y-1) + index(source, x, y-1) + index(source, x+1, y-1)
+                         + index(source, x-1, y  ) + index(source, x, y  ) + index(source, x+1, y  )
+                         + index(source, x-1, y+1) + index(source, x, y+1) + index(source, x+1, y+1);
             new: bool = match count {
                 n if n==3 => true,
                 n if n==4 => true,
                 _         => false,
-            }
-            assign(buffer, x, y, new);
+            };
+            assign(destination, x, y, new);
         }
     }
 }
@@ -74,14 +84,14 @@ fn main () {
     println!("John Conway's Game of Life");
     
     // load initial world
-    let buffer1 = load("initial.map");
-    let buffer2 = blank(buffer1);
+    let world1 = load("initial.map");
+    let world2 = blank(world1);
     
     // iterate
     while true {
-        show(buffer1);
-        iterate(buffer1, buffer2);
-        show(buffer2);
-        iterate(buffer2, buffer1):
+        show(world1);
+        iterate(world1, world2);
+        show(world2);
+        iterate(world2, world1);
     }
 }
